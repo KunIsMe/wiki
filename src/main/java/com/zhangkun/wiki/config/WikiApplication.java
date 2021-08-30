97d64820cdee4e50c198160acad4cedba0bd2e99
‘zhangkun’ <1400068385@qq.com>
2021-08-30 13:38:37 +0800

    1.5 开发 Hello World 接口 访问地址：http://127.0.0.1:8880/hello

--- src/main/java/com/zhangkun/wiki/config/WikiApplication.java
+++ src/main/java/com/zhangkun/wiki/config/WikiApplication.java
@@ -1,11 +1,13 @@
-package com.zhangkun.wiki;
+package com.zhangkun.wiki.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
+@ComponentScan("com.zhangkun")
 @SpringBootApplication
 public class WikiApplication {
 
