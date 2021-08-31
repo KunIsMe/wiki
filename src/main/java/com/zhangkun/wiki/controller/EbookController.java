1403e83c6229a3b8244afce493095948e4d7c120
‘zhangkun’ <1400068385@qq.com>
2021-08-31 15:33:59 +0800

    1.15 封装请求参数和返回参数

--- src/main/java/com/zhangkun/wiki/controller/EbookController.java
+++ src/main/java/com/zhangkun/wiki/controller/EbookController.java
@@ -1,7 +1,8 @@
 package com.zhangkun.wiki.controller;
 
-import com.zhangkun.wiki.domain.Ebook;
+import com.zhangkun.wiki.req.EbookReq;
 import com.zhangkun.wiki.resp.CommonResp;
+import com.zhangkun.wiki.resp.EbookResp;
 import com.zhangkun.wiki.service.EbookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,9 +19,9 @@ public class EbookController {
     private EbookService ebookService;
 
     @GetMapping("/list")
-    public CommonResp list() {
-        CommonResp<List<Ebook>> resp = new CommonResp<>();
-        List<Ebook> list = ebookService.list();
+    public CommonResp list(EbookReq req) {
+        CommonResp<List<EbookResp>> resp = new CommonResp<>();
+        List<EbookResp> list = ebookService.list(req);
         resp.setContent(list);
         return resp;
     }
--- src/main/java/com/zhangkun/wiki/service/EbookService.java
+++ src/main/java/com/zhangkun/wiki/service/EbookService.java
@@ -1,10 +1,15 @@
 package com.zhangkun.wiki.service;
 
 import com.zhangkun.wiki.domain.Ebook;
+import com.zhangkun.wiki.domain.EbookExample;
 import com.zhangkun.wiki.mapper.EbookMapper;
+import com.zhangkun.wiki.req.EbookReq;
+import com.zhangkun.wiki.resp.EbookResp;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -13,7 +18,18 @@ public class EbookService {
     @Autowired
     private EbookMapper ebookMapper;
 
-    public List<Ebook> list() {
-        return ebookMapper.selectByExample(null);
+    public List<EbookResp> list(EbookReq req) {
+        EbookExample ebookExample = new EbookExample();
+        EbookExample.Criteria criteria = ebookExample.createCriteria();
+        criteria.andNameLike("%" + req.getName() + "%");
+        List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
+
+        List<EbookResp> ebookRespList = new ArrayList<>();
+        for (Ebook ebook : ebookList) {
+            EbookResp ebookResp = new EbookResp();
+            BeanUtils.copyProperties(ebook, ebookResp);
+            ebookRespList.add(ebookResp);
+        }
+        return ebookRespList;
     }
 }
