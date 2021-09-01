2a4d7cfd719004cc6d9b47ea8f557f6d4aad5e04
‘zhangkun’ <1400068385@qq.com>
2021-09-01 13:48:23 +0800

    1.23 使用 axios 调用后端接口 /ebook/list，并解决前后端跨域问题 CorsConfig

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -55,8 +55,14 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import axios from 'axios';
 
 export default defineComponent({
-  name: 'Home'
+  name: 'Home',
+  setup () {
+    axios.get("http://localhost:8880/ebook/list?name=Spring").then((response) => {
+      console.log(response);
+    })
+  }
 });
 </script>
