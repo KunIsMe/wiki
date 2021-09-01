79d3535198eba519213fa2cb8bd524eebd08fc5c
‘zhangkun’ <1400068385@qq.com>
2021-09-01 17:00:40 +0800

    1.31 修改 axios 的 baseURL 读配置文件

--- web/src/main.ts
+++ web/src/main.ts
@@ -5,6 +5,9 @@ import store from './store'
 import Antd from 'ant-design-vue'
 import 'ant-design-vue/dist/antd.css'
 import * as Icons from '@ant-design/icons-vue'
+import axios from 'axios'
+
+axios.defaults.baseURL = process.env.VUE_APP_SERVER;
 
 const app = createApp(App);
 app.use(store).use(router).use(Antd).mount('#app');
@@ -14,6 +17,3 @@ const icons: any = Icons;
 for (const i in icons) {
     app.component(i, icons[i]);
 }
-
-console.log('环境：', process.env.NODE_ENV);
-console.log('服务端：', process.env.VUE_APP_SERVER);
