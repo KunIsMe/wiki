005fadac8838d7b9eb0342757bf8db24feaaaad7
‘zhangkun’ <1400068385@qq.com>
2021-09-01 16:52:24 +0800

    1.30 axios 请求域改为读配置文件

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -81,7 +81,7 @@ export default defineComponent({
     const ebooks2 = reactive({ ebooks: [] });
 
     onMounted(() => {
-      axios.get("http://localhost:8880/ebook/list").then((response) => {
+      axios.get(process.env.VUE_APP_SERVER + "/ebook/list").then((response) => {
         const data = response.data;
         ebooks1.value = data.content;
         ebooks2.ebooks = data.content;
