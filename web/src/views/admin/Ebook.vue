ed758b38987d28307e39b59343f7cb6bc37b65e5
‘zhangkun’ <1400068385@qq.com>
2021-09-02 15:07:54 +0800

    1.37 修改电子书管理页面布局

--- web/src/views/admin/Ebook.vue
+++ web/src/views/admin/Ebook.vue
@@ -1,13 +1,106 @@
 <template>
-  <div class="about">
-    <h1>电子书管理</h1>
-  </div>
+  <a-layout>
+      <a-layout-content
+        :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
+      >
+        <a-table :columns="columns" :data-source="data">
+          <template #name="{ text }">
+            <a>{{ text }}</a>
+          </template>
+          <template #customTitle>
+            <span>
+              <smile-outlined />
+              Name
+            </span>
+          </template>
+          <template #tags="{ text: tags }">
+            <span>
+              <a-tag
+                v-for="tag in tags"
+                :key="tag"
+                :color="tag === 'loser' ? 'volcano' : tag.length > 5 ? 'geekblue' : 'green'"
+              >
+                {{ tag.toUpperCase() }}
+              </a-tag>
+            </span>
+          </template>
+          <template #action="{ record }">
+            <span>
+              <a>Invite 一 {{ record.name }}</a>
+              <a-divider type="vertical" />
+              <a>Delete</a>
+              <a-divider type="vertical" />
+              <a class="ant-dropdown-link">
+                More actions
+                <down-outlined />
+              </a>
+            </span>
+          </template>
+        </a-table>
+      </a-layout-content>
+    </a-layout>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 
+const columns = [
+  {
+    dataIndex: 'name',
+    key: 'name',
+    slots: { title: 'customTitle', customRender: 'name' },
+  },
+  {
+    title: 'Age',
+    dataIndex: 'age',
+    key: 'age',
+  },
+  {
+    title: 'Address',
+    dataIndex: 'address',
+    key: 'address',
+  },
+  {
+    title: 'Tags',
+    key: 'tags',
+    dataIndex: 'tags',
+    slots: { customRender: 'tags' },
+  },
+  {
+    title: 'Action',
+    key: 'action',
+    slots: { customRender: 'action' },
+  },
+];
+
+const data = [
+  {
+    key: '1',
+    name: 'John Brown',
+    age: 32,
+    address: 'New York No. 1 Lake Park',
+    tags: ['nice', 'developer'],
+  },
+  {
+    key: '2',
+    name: 'Jim Green',
+    age: 42,
+    address: 'London No. 1 Lake Park',
+    tags: ['loser'],
+  },
+  {
+    key: '3',
+    name: 'Joe Black',
+    age: 32,
+    address: 'Sidney No. 1 Lake Park',
+    tags: ['cool', 'teacher'],
+  },
+];
+
 export default defineComponent({
-  name: 'Ebook'
+  name: 'Ebook',
+  setup () {
+    return { data, columns }
+  }
 })
 </script>
