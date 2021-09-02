ec8d391c73eeae90066b30228c2641e54a5c1d13
‘zhangkun’ <1400068385@qq.com>
2021-09-02 14:02:51 +0800

    1.36 增加电子书管理页面。 新页面开发：页面、路由、菜单

--- web/src/views/admin/Ebook.vue
+++ web/src/views/admin/Ebook.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about">
-    <h1>关于我们</h1>
+    <h1>电子书管理</h1>
   </div>
 </template>
 
@@ -8,6 +8,6 @@
 import { defineComponent } from 'vue';
 
 export default defineComponent({
-  name: 'About'
+  name: 'Ebook'
 })
 </script>
