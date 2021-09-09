8f7eb1c0d2addedfc177956c182476bfaf2abbeb
‘zhangkun’ <1400068385@qq.com>
2021-09-09 12:38:23 +0800

    1.51 分类表设计与持久层代码生成 基本的无限级树型表结构设计：id+parent

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -16,6 +16,7 @@ module.exports = {
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'vue/no-unused-components' : 'off',
     '@typescript-eslint/no-explicit-any' : 'off',
-    'vue/no-unused-vars': 0
+    'vue/no-unused-vars': 0,
+    '@typescript-eslint/explicit-module-boundary-types': 0
   }
 }
