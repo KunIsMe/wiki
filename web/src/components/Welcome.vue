ab99f84b5935d565b6027ac2a34f79f6c4dd1331
‘zhangkun’ <1400068385@qq.com>
2021-11-08 22:37:43 +0800

    1.128 集成Echarts，增加示例

--- web/src/components/Welcome.vue
+++ web/src/components/Welcome.vue
@@ -86,11 +86,18 @@
       </a-card>
     </a-col>
   </a-row>
+  <br />
+  <a-row>
+    <a-col :span="24" id="main-col">
+      <div id="main" style="width:85%;height:300px;"></div>
+    </a-col>
+  </a-row>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref, onMounted } from 'vue';
 import axios from 'axios';
+import * as echarts from 'echarts';
 
 export default defineComponent({
   name: 'Welcome',
@@ -131,13 +138,43 @@ export default defineComponent({
             statistic.value.todayViewIncreaseRate = 100;
             statistic.value.todayViewIncreaseRateAbs = Math.abs(statistic.value.todayViewIncreaseRate);
           }
-          
         }
       });
     };
 
+    const testEcharts = () => {
+      // 基于准备好的dom，初始化echarts实例
+      const myChart = echarts.init(document.getElementById('main') as HTMLElement);
+
+      // 指定图表的配置项和数据
+      const option = {
+        title: {
+          text: 'ECharts 入门示例'
+        },
+        tooltip: {},
+        legend: {
+          data: ['销量']
+        },
+        xAxis: {
+          data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+        },
+        yAxis: {},
+        series: [
+          {
+            name: '销量',
+            type: 'bar',
+            data: [5, 20, 36, 10, 10, 20]
+          }
+        ]
+      };
+
+      // 使用刚指定的配置项和数据显示图表。
+      myChart.setOption(option);
+    }
+
     onMounted(() => {
       getStatistic();
+      testEcharts();
     })
 
     return { statistic }
